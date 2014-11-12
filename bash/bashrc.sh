ac36d6f06bcb9509356dbe3734781c719ec0fb21
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-11-12 09:18:27 +0100

    Include ~/bin in bashrc instead of profile

--- bash/bashrc.sh
+++ bash/bashrc.sh
@@ -24,6 +24,11 @@ if [ -f "/etc/bash_completion" ]; then
 . "/etc/bash_completion"
 fi
 
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+  PATH=$PATH:$HOME/bin
+fi
+
 # Show git repository in command prompt
 export GIT_PS1_SHOWDIRTYSTATE=1
 export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\]$(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] '
