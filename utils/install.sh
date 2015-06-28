95592c963457f70c88cbfcacee7ecb35c69fb085
Sondre Lefsaker <sondrele@stud.ntnu.no>
2015-06-28 18:46:01 +0200

    Link i3-exit to bin-directory

--- utils/install.sh
+++ utils/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir -p "$INSTALL_DIR/.i3"
+mkdir -p "$INSTALL_DIR/bin"
 
-ln -sf "$SRC_DIR/utils/i3-exit.py" "$INSTALL_DIR/.i3/i3-exit.py"
+ln -sf "$SRC_DIR/utils/i3-exit.py" "$INSTALL_DIR/bin/i3-exit"
