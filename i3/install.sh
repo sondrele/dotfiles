4f8df6afc48496e165835139450c4cba8de9fbb7
Sondre Lefsaker <sondrele@stud.ntnu.no>
2015-11-12 12:04:23 +0100

    Add a couple of terminal layouts and a script to initiate them

--- i3/install.sh
+++ i3/install.sh
@@ -4,7 +4,12 @@ DIR="$INSTALL_DIR/.i3"
 
 mkdir -p $DIR
 
+# configs
 ln -sf "$SRC_DIR/i3/config" "$DIR/config"
 ln -sf "$SRC_DIR/i3/i3status.conf" "$DIR/i3status.conf"
 ln -sf "$SRC_DIR/i3/i3status-small.conf" "$DIR/i3status-small.conf"
-ln -sf "$SRC_DIR/i3/workspace-wwww.json" "$DIR/i3/workspace-wwww.json"
+
+# layouts
+ln -sf "$SRC_DIR/i3/workspace-www.json" "$DIR/workspace-www.json"
+ln -sf "$SRC_DIR/i3/terminal-2.json" "$DIR/terminal-2.json"
+ln -sf "$SRC_DIR/i3/terminal-3.json" "$DIR/terminal-3.json"
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,4 @@
 mkdir -p "$INSTALL_DIR/bin"
 
 ln -sf "$SRC_DIR/scripts/fuzzy-i3lock.sh" "$INSTALL_DIR/bin/fuzzy-i3lock"
+ln -sf "$SRC_DIR/scripts/terminal-layout.sh" "$INSTALL_DIR/bin/terminal-layout"
