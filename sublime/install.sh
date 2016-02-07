f85111b83f14ad6e4338b2c24eea3e0f09812333
Sondre Lefsaker <sondrele@stud.ntnu.no>
2016-02-07 16:15:18 +0100

    refactored sublime User package configuration to symlink the entire directory

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-DIR="$INSTALL_DIR/.config/sublime-text-3/Packages/User"
+DIR="$INSTALL_DIR/.config/sublime-text-3/Packages"
 
 mkdir -p $DIR
 
-ln -sf "$SRC_DIR/sublime/Default (Linux).sublime-keymap"       "$DIR/Default (Linux).sublime-keymap"
-ln -sf "$SRC_DIR/sublime/Package Control.sublime-settings"     "$DIR/Package Control.sublime-settings"
-ln -sf "$SRC_DIR/sublime/resize_active_group.sublime-settings" "$DIR/resize_active_group.sublime-settings"
-ln -sf "$SRC_DIR/sublime/trailing_spaces.sublime-settings"     "$DIR/trailing_spaces.sublime-settings"
-ln -sf "$SRC_DIR/sublime/Preferences.sublime-settings"         "$DIR/Preferences.sublime-settings"
+# symlink entire user directory
+ln -sf "$SRC_DIR/sublime/User"       "$DIR/User"
