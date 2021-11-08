411197dd8e3e87abd8931dc047c52a5780977d5f
Sondre Lefsaker <sondre.lefsaker@nav.no>
2021-11-08 23:02:41 +0100

    change install of sshrc

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,4 +3,4 @@
 mkdir -p "$INSTALL_DIR/bin"
 
 ln -sf "$SRC_DIR/scripts/fuzzy-i3lock.sh" "$INSTALL_DIR/bin/fuzzy-i3lock"
-ln -sf "$SRC_DIR/scripts/terminal-layout.sh" "$INSTALL_DIR/bin/terminal-layout"
+ln -sf "$SRC_DIR/scripts/sshrc.sh" "$INSTALL_DIR/bin/sshrc"
